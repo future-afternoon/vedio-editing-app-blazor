0a08480b260087c3792f530735af8859fec7fd0f
ABDUL REHMAN <152426209+future-afternoon@users.noreply.github.com>
2024-01-16 21:09:12 +0500

    Add files via upload

---
+++ video-editing-app-blazor/wwwroot/json/Pause_Play.js
@@ -0,0 +1,21 @@
+﻿// custom.js
+function playVideo() {
+    var video = document.getElementById("videoPlayer");
+    if (video) {
+        video.play();
+    }
+}
+
+function pauseVideo() {
+    var video = document.getElementById("videoPlayer");
+    if (video) {
+        video.pause();
+    }
+}
+
+function rewindVideo() {
+    var video = document.getElementById("videoPlayer");
+    if (video) {
+        video.currentTime = 0;
+    }
+}
